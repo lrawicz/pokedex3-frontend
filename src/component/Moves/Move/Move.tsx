9162f6c090689021a146684e46e21eca8bab9daf
lrawicz <lrawicz@gmail.com>
2024-03-15 19:01:12 -0300

    review filter for moves

--- src/component/Moves/Move/Move.tsx
+++ src/component/Moves/Move/Move.tsx
@@ -80,29 +80,29 @@ export const Move:  React.FC<params>  = ({callback,moveId}) => {
           <Row>
             <Col span={6} children={<SelectMultiCheck dbName="target" label="target" callback={callFromChild} urlSource={`${process.env.REACT_APP_API_URL}/moves/getTargetTypes`} />}/>
             <Col span={6} children={<RangedCollapse dbName="priority" label="priority" step={1} marks={[-7,0,8]} min={-7} max={8} callback={callFromChild}/>} />
-            <Col span={6} children={<RangedCollapse dbName="CritRate" label="indice de critico" step={1} min={0} max={6} marks={[0,3,6]} callback={callFromChild}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaCritRate" label="indice de critico" step={1} min={0} max={6} marks={[0,3,6]} callback={callFromChild}/>}/>
             <Col span={6} children={<RangedCollapse dbName="pp" label="pp"  min={1} max={40} step={5} marks={[1,20,40]}callback={callFromChild}/> }/>
           </Row>
           
           <Row>
-            <Col span={6} children={<RangedCollapse dbName="minHits" label="minHits"  min={1} max={10} callback={callFromChild}/>}/>
-            <Col span={6} children={<RangedCollapse dbName="maxHits" label="maxHits"  callback={callFromChild} min={1} max={10}/>}/>
-            <Col span={6} children={<RangedCollapse dbName="minTurns" label="minTurns"  callback={callFromChild} min={1} max={20}/>}/>
-            <Col span={6} children={<RangedCollapse dbName="maxTurns" label="maxTurns"  callback={callFromChild} min={1} max={20}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaMinHits" label="minHits"  min={1} max={10} step={1} marks={[1,5,10]} callback={callFromChild}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaMaxHits" label="maxHits"   min={1} max={10} step={1} marks={[1,5,10]} callback={callFromChild} />}/>
+            <Col span={6} children={<RangedCollapse dbName="metaMinTurns" label="minTurns"  min={1} max={20} step={1} marks={[1,10,20]}  callback={callFromChild}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaMaxTurns" label="maxTurns" min={1} max={20} step={1} marks={[1,10,20]}  callback={callFromChild} />}/>
           </Row>
     
           <Row>
-            <Col span={6} children={<InputNumberCheck dbName="drain" label="drain" callback={callFromChild} min={0} max={100}/>}/>
-            <Col span={6} children={<InputNumberCheck dbName="healing" label="healing" callback={callFromChild} />}/>
-            <Col span={6} children={<InputNumberCheck dbName="recoil" label="recoil"  callback={callFromChild} min={0} max={100}/>}/>
-            <Col span={6} children={<InputNumberCheck dbName="maxTurns" label="flinchChance" callback={callFromChild} min={0} max={100}/>}/>
+            <Col span={6} style={{background:"red"}} children={<InputNumberCheck dbName="drain" label="drain" callback={callFromChild} min={0} max={100}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaHealing" label="healing" min={0} max={100} step={1} marks={[0,50,100]} callback={callFromChild} />}/>
+            <Col span={6} style={{background:"red"}} children={<InputNumberCheck dbName="recoil" label="recoil"  callback={callFromChild} min={0} max={100}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaFlinchChance" label="flinchChance" min={0} max={100} step={1} marks={[0,50,100]} callback={callFromChild} />}/>
           </Row>
 
           <Row>
-            <Col span={6} children={<SelectMultiCheck dbName="ailment" label="ailment"  callback={callFromChild} urlSource={`${process.env.REACT_APP_API_URL}/moves/getAilments`}/>}/>
-            <Col span={6} children={<InputNumberCheck dbName="ailmentChance" label="ailmentChance" callback={callFromChild} min={0} max={100}/>}/>
-            {/*<Col span={6} children={<InputNumberCheck dbName="metaStatChanges" label="statChanges"  callback={callFromChild} />}/> {/*revisar*/}
-            <Col span={6} children={<InputNumberCheck dbName="metaStatChance" label="statChance" callback={callFromChild} min={0} max={100}/>}/>
+            <Col span={6} children={<SelectMultiCheck dbName="metaAilment" label="ailment"  callback={callFromChild} urlSource={`${process.env.REACT_APP_API_URL}/moves/getAilments`}/>}/>
+            <Col span={6} children={<RangedCollapse dbName="metaAilmentChance" label="ailmentChance" min={0} max={100} marks={[0,50,100]}callback={callFromChild}  />}/>
+            <Col span={6} style={{background:"red"}} children={<InputNumberCheck dbName="metaStatChanges" label="statChanges"  callback={callFromChild} />}/> {/*revisar*/}
+            <Col span={6} children={<RangedCollapse dbName="metaStatChance" label="statChance" min={0} max={100} marks={[0,50,100]} callback={callFromChild} />}/>
           </Row>
     </>
       )
